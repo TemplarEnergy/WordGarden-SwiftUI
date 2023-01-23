899ed8455ef5f8a85f7b90ee7d646f2a887e3666
Tom Radford <tom@radfords-web.com>
2023-01-23 16:19:39 -0400

    Initial Commit

---
+++ WordGarden-SwiftUI/WordGarden_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_SwiftUIApp.swift
+//  WordGarden-SwiftUI
+//
+//  Created by Thomas Radford on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
